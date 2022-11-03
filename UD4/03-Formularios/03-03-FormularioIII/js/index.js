8135510cf7ee51ac3441aa7bf8030ebbc65045c1
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2022-11-03 10:56:58 +0100

    Add files via upload

--- UD4/03-Formularios/03-03-FormularioIII/js/index.js
+++ UD4/03-Formularios/03-03-FormularioIII/js/index.js
@@ -1,35 +1,108 @@
 "use strict";
-//declaración de función
+//declaración de funciones y variables
+let nombre, edad, tipo, provincia;
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("frmSec").addEventListener("submit", ejecutar);
+  //establecer el objeto formulario
+  const formulario = document.getElementById("frmFirst");
+
+  //establecer objetos del formulario
+  nombre = document.getElementById("nombre");
+  edad = document.getElementById("edad");
+  tipo = document.getElementById("tipo");
+  provincia = document.getElementById("provincia");
+
+  //establecer eventos
+  formulario.addEventListener("submit", enviar);
+  formulario.addEventListener("reset", () => {
+    limpiar();
+  });
+  edad.addEventListener("blur", mayorEdad);
 });
 
-const ejecutar = (e) => {
-  let mostrar = document.getElementById("mostrar");
-  //mostrar el nº de formularios del body
-  mostrar.innerHTML = `El nº de formularios es ${document.forms.length}`;
-  //mostrar el nº de elementos de cada formulario
-  mostrar.innerHTML += `<br>Los elementos del ${
-    document.getElementsByTagName("form")[0].id
-  } son ${document.getElementById("frmFirst").elements.length}`;
-  mostrar.innerHTML += `<br>Los elementos del ${document.forms[1].id} son ${
-    document.getElementById("frmSec").elements.length
-  }`;
+function mayorEdad(e) {
+  // console.log(edad.value);
 
-  //mostrar todos los elementos de los diferentes formularios que hay dentro del body
-
-  for (let frm of document.forms) {
-    //recorrer todos los formularios del body
-    mostrar.innerHTML += `<hr><br><br>Formulario ${frm.id}`;
-    //mostrar los elementos
-
-    for (let ele of frm.elements) {
-      //recorrer todos los elementos del formulario
-      mostrar.innerHTML += `<br> El id es ${ele.id}`;
-      mostrar.innerHTML += `<br> El type es ${ele.type}`;
-      mostrar.innerHTML += `<br> El valor es ${ele.value}`;
-    }
+  // console.log(e.target.value); //otra forma de acceder
+  // console.log(this.value);
+  const error = document.getElementById("erredad");
+  if (this.value < 18) {
+    error.innerText = "La edad debe ser mayor o igual a 18 años";
+    this.focus(); //mantiene el foco
+  } else {
+    error.innerText = ""; //quitar el texto de error
   }
+}
 
-  e.preventDefault();
+const enviar = (e) => {
+ const inputs=validarText();
+ const tipo= validarTipo();
+ 
+   if (!inputs && !tipo){ //NO hay datos vacíos
+  //   //mostrar spinner
+     const spinner=document.getElementById("spinner");
+     let exito=document.getElementById("exito");
+     spinner.style.display='block';//visible el spinner
+     exito.innerText='Enviando datos ....'; //mostrar el texto
+    
+  //   //limpiar spinner
+     setTimeout(() => {
+       spinner.style.display='none'; //no mostrar el spinner
+       exito.innerText='';//limpiar el mensaje
+       limpiar();
+  }, 3000);
+
+  }
+  //todo correcto, mostrar el spinner durante 3 segundos y el texto 'Enviando datos'
+  //después limpiar el formulario, sin utilizar reload();
+
+  e.preventDefault(); //anula el evento submit
 };
+const validarText = () => {
+  const objetosText = document.getElementsByName("texto");
+  let errores=false;
+  objetosText.forEach((elemento) => {
+    const error = document.getElementById(`err${elemento.id}`);
+    if (elemento.value.trim().length == 0) {
+      error.innerText = "El campo es requerido";
+      errores=true;
+    } else {
+      error.innerText = "";
+    }
+  });
+  return errores;
+};
+
+const validarTipo = () => {
+  const objetosTipo = document.getElementsByName("tipo");
+  const error = document.getElementById(`errtipo`);
+  let checked = false;
+  objetosTipo.forEach((elemento) => {
+    if (elemento.checked) {
+      checked = true;
+    }
+  });
+  if (checked==false) {
+    error.innerText = "El campo es requerido";
+    return true;
+  } else {
+    error.innerText = "";
+    return false;
+  }
+  
+};
+
+const limpiar=()=>{
+  const spans=document.getElementsByName("error");
+  const tipos=document.getElementsByName("tipo");
+  const inputs=document.getElementsByName("texto");
+  spans.forEach(elemento=>{
+    elemento.innerText='';//limpiar los span
+    
+  })
+  inputs.forEach(elemento=>{
+    elemento.value="";
+  })
+  tipos.forEach(elemento=>{
+    elemento.checked=false;
+  })
+}
