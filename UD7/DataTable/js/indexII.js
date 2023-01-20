a7813529fb998f61f4754896044c0e96fdc16764
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2023-01-20 12:49:29 +0100

    Add files via upload

---
+++ UD7/DataTable/js/indexII.js
@@ -0,0 +1,46 @@
+"use strict"
+document.addEventListener("DOMContentLoaded", ()=>{
+    mostrarTabla();
+    
+})
+const mostrarTabla=async()=>{
+   
+       $('.table').DataTable({
+        'ajax':{
+           url:'../AjaxJquery/EjemploBD/php/mostrar.php',
+           type:'GET',
+           data:{
+            perro:''
+           },
+           dataType:'json'
+        },
+        columns:[{
+            data: "chip"
+        },
+        {
+            data: "nombre"
+        },
+        {
+            data: "raza"
+        },
+        {
+            data: "fechaNac"
+        }
+        ],
+        'language':{
+            url:'../assets/librerias/DataTables/es-Es.json'
+        },
+        'sPaginationType':'full_numbers', //muestra los botones First y last
+        'bDestroy' :true,
+        'initComplete':function(settings, json){
+            
+                $('.table tbody tr').on('click', function(){
+                    $(this).toggleClass("selected");
+                })
+           
+        }
+        
+    });
+    
+    
+}
